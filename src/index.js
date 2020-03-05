12de2a0e2603f44d078691afdce3bda5801ae427
JatuphumS <jatuphum.s@gmail.com>
2020-03-05 16:53:58 +0700

    start redux

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom'
 import {
     BrowserRouter
 } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 import './index.css'
 import App from './App'
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-
+    <Provider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
     , document.getElementById('root'));
